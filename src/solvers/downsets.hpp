a5ed88254a47997c6d995566270c2d3ffde93c7a
Michaël Cadilhac <michael@cadilhac.name>
2024-08-21 17:02:58 -0500

    Implement downset-based approach.

---
+++ src/solvers/downsets.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <ranges>
+#include "solver.hpp"
+
+#include "posets/downsets.hh"
+#include "posets/vectors.hh"
+
+namespace pg {
+
+  class DownsetsSolver : public Solver {
+      using vertex_t = int32_t;
+      using weight_t = int16_t;
+      using vector_t = posets::vectors::vector_backed<weight_t>;
+      using downset_t = posets::downsets::kdtree_backed<vector_t>;
+    public:
+      DownsetsSolver (Oink* oink, Game* game);
+      virtual ~DownsetsSolver ();
+      virtual void run ();
+
+    private:
+      const Game& pgame;
+      const size_t dim;
+      const std::ranges::iota_view<vertex_t, vertex_t> all_verts;
+      std::vector<weight_t> maxes;
+      vector_t max_vec;
+      std::vector<downset_t> cur;
+      void init_maxes (bool swap = false);
+      void init_downsets (bool swap = false);
+      bool compute_cpre (bool swap = false);
+  };
+}
